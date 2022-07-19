b9257eb90ec2b9ff5cfbd9cae10b072f72295607
adnan <me@adnan.io>
2022-07-19 12:51:23 -0500

    added basic CRUD controller scaffold

---
+++ src/form/dto/create-form.dto.ts
@@ -0,0 +1,9 @@
+export class CreateFormDto {
+    readonly firstName: string;
+    readonly lastName: string;
+    readonly address: string;
+    readonly city: string;
+    readonly state: string;
+    readonly zipCode: string;
+    readonly email: string;
+}
--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -1,5 +1,46 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post, 
+    Put, 
+    Delete, 
+    Body, 
+    Param 
+} from '@nestjs/common';
+import { CreateFormDto } from './dto/create-form.dto';
 
 @Controller('form')
 export class FormController {
+    @Get()
+    findAll(): string {
+        return 'Get all';
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id): string {
+        return `Item ${id}`;
+    }
+
+    @Post()
+    create(@Body() createFormDto: CreateFormDto): string {
+        return `
+            firstName: ${createFormDto.firstName}
+            lastName: ${createFormDto.lastName}
+            address: ${createFormDto.address}
+            city: ${createFormDto.city}
+            state: ${createFormDto.state}
+            zipCode: ${createFormDto.zipCode}
+            email: ${createFormDto.email}
+            `;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): string {
+        return `deleted ${id}`;
+    }
+
+    @Put(':id')
+    update(@Body() updateFormDto: CreateFormDto, @Param('id') id): string {
+        return `update ${id}`;
+    }
 }
